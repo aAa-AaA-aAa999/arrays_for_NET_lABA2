26d446250643d173f9550428ce14dcab0c8944f3
7x7x49.mrr <98632330+kitty7x7x49@users.noreply.github.com>
2023-02-17 11:09:39 +0300

    C#
    C#

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива:");
+            int razmer = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[razmer];
+            Console.WriteLine("Введите целые числа массива:");
+            for (int i = 0; i < razmer; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int sravnenie = array[i]; 
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > sravnenie)
+                {
+                    array[j + 1] = array[j];
+                    array[j] = sravnenie;
+                    j--;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Результат:");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < array.Length; i++)
+            {
+                    Console.Write(array[i] + "  ");
+            }
+        }
+    }
+}
